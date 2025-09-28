1d765aba6bcfb6e2f585cafc669528d2c028cdc2
arceniovilca <arcenio.vilca@acidlabs.com>
2025-09-27 23:12:57 -0500

    chore: add global styles and design tokens from Figma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,46 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: {
+          primary: "#FFFFFF",
+          secondary: "#EEEEEE",
+          footer: "#404040",
+        },
+        text: {
+          primary: "#3B3B3B",
+          secondary: "#737373",
+        },
+        border: {
+          secondary: "#8F8F8F",
+        },
+        cta: {
+          bg: "#585660",
+          text: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        logo: ["'Area Normal'", "sans-serif"],
+        archivo: ["Archivo", "sans-serif"],
+      },
+      fontSize: {
+        h1: ["36px", { lineHeight: "40px", fontWeight: "700" }],
+        h2: ["24px", { lineHeight: "28px", fontWeight: "700" }],
+        bodyLg: ["24px", { lineHeight: "28px", fontWeight: "400" }],
+        bodyMd: ["18px", { lineHeight: "24px", fontWeight: "400" }],
+        bodySm: ["16px", { lineHeight: "16px", fontWeight: "500" }],
+        price: ["18px", { lineHeight: "20px", fontWeight: "700" }],
+        button: [
+          "16px",
+          { lineHeight: "16px", fontWeight: "700", letterSpacing: "0.5px" },
+        ],
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1280px",
+        "2xl": "1536px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -17,4 +57,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
